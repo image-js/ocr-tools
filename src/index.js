3ce2843399b5322683aa4b8172fa2d57788fe1d5
Michaël Zasso <targos@protonmail.com>
2018-01-29 13:07:26 +0100

    export more

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ module.exports = {
   getLinesFromImage: require('./util/getLinesFromImage'),
   doOcrOnLines: require('./util/doOcrOnLines'),
   groupRoisPerLine: require('./util/groupRoisPerLine'),
-  runFontAnalysis: require('./runFontAnalysis')
+  runFontAnalysis: require('./runFontAnalysis'),
+  loadAllFontData: require('./util/loadAllFontData'),
+  loadFontData: require('./util/loadFontData')
 };
--- src/util/symbolClasses.js
+++ src/util/symbolClasses.js
@@ -16,17 +16,7 @@ for (let i = '0'.charCodeAt(0); i <= '9'.charCodeAt(0); i++) {
   NUMBERS.symbols.push(i);
 }
 
-const MRZ = { symbols: [], label: 'mrz' };
-for (let i = '0'.charCodeAt(0); i <= '9'.charCodeAt(0); i++) {
-  MRZ.symbols.push(i);
-}
-for (let i = 'A'.charCodeAt(0); i <= 'Z'.charCodeAt(0); i++) {
-  MRZ.symbols.push(i);
-}
-MRZ.symbols.push('<'.charCodeAt(0));
-
 module.exports = {
   LETTERS_NUMBERS,
-  NUMBERS,
-  MRZ
+  NUMBERS
 };
