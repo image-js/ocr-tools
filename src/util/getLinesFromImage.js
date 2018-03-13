0da7811673c55bed9c0c1b2993ea6590c7b16f8b
Daniel Kostro <kostro.d@gmail.com>
2018-03-13 17:50:00 +0100

    better filtering of rois

--- src/util/getLinesFromImage.js
+++ src/util/getLinesFromImage.js
@@ -1,7 +1,9 @@
-"use strict";
+'use strict';
 
-var groupRoisPerLine = require("./groupRoisPerLine");
-const mean = require("ml-array-mean");
+const Image = require('image-js').Image;
+var groupRoisPerLine = require('./groupRoisPerLine');
+const mean = require('ml-array-mean');
+const median = require('ml-array-median');
 
 module.exports = function getLinesFromImage(image, options = {}) {
   const { roiOptions, fingerprintOptions } = options;
@@ -24,21 +26,28 @@ module.exports = function getLinesFromImage(image, options = {}) {
     }
     maskOptions.threshold = greyThreshold;
   } else {
-    throw new Error("no algorithm or greyThreshold provided to apply.");
+    throw new Error('no algorithm or greyThreshold provided to apply.');
   }
 
+  // let mask = getMask(grey, maskOptions);
   var mask = grey.mask(maskOptions);
 
   var manager = image.getRoiManager();
   manager.fromMask(mask);
   // TODO: change this options until it works
   var rois = manager.getRois(roiOptions);
-  var averageSurface = mean(rois.map(elem => elem.surface));
+  var averageSurface = mean(rois.map((elem) => elem.surface));
+  var medianSurface = median(rois.map((elem) => elem.surface));
   var painted = manager.paint(roiOptions);
 
-  rois = rois.filter(roi => roi.width !== 1 || roi.height !== 1);
+  rois = rois.filter((roi) => roi.width !== 1 || roi.height !== 1);
+  rois = rois.filter(
+    (roi) => roi.surface * 3 > medianSurface && roi.surface / 3 < medianSurface
+  );
 
-  rois.forEach(function(roi) {
+  console.log('number of rois', rois.length);
+
+  rois.forEach(function (roi) {
     var small = roi.getMask().scale({
       width: fingerprintOptions.width,
       height: fingerprintOptions.height
@@ -54,7 +63,7 @@ module.exports = function getLinesFromImage(image, options = {}) {
     roi.mbrSurface = roi.mbrWidth * roi.mbrHeight;
     roi.fillingFactor = roi.surface / roi.mbrSurface;
 
-    mbr = mbr.map(point => [
+    mbr = mbr.map((point) => [
       point[0] + mask.position[0],
       point[1] + mask.position[1]
     ]);
@@ -71,4 +80,35 @@ module.exports = function getLinesFromImage(image, options = {}) {
 
 function getDistance(p1, p2) {
   return Math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2);
+}
+
+function getMask(image, maskOptions) {
+  let mask = new Image(image.width, image.height, { kind: 'BINARY' });
+  const partsY = 4;
+  const partsX = 30;
+  const h = Math.floor(image.height / partsY);
+  const w = Math.floor(image.width / partsX);
+  for (let i = 0; i < partsX; i++) {
+    for (let j = 0; j < partsY; j++) {
+      let x = i * w;
+      let y = j * h;
+      let width = w;
+      let height = h;
+      if (i === partsX - 1) {
+        width += image.width % partsX;
+      }
+      if (j === partsY - 1) {
+        height += image.height % partsY;
+      }
+      const params = {
+        x,
+        y,
+        width,
+        height
+      };
+      const imagePart = image.crop(params).mask(maskOptions);
+      mask.insert(imagePart, { inPlace: true, offsetX: x, offsetY: y });
+    }
+  }
+  return mask;
 }
